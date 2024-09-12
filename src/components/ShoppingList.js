07a556a91683cef9228e9e66e5c46e52222b2015
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-12 12:48:51 +0300

    complete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,38 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+class ShoppingList extends React.Component {
+  state = {
+    selectedCategory: "All",
+  };
 
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
-  }
+  handleCategoryChange = (event) => {
+    this.setState(() => {
+      return { selectedCategory: event.target.value };
+    });
+  };
 
-  // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  render() {
+    const itemsToDisplay = this.props.items.filter((item) => {
+      if (this.state.selectedCategory === "All") {
+        return true;
+      } else {
+        return item.category === this.state.selectedCategory;
+      }
+    });
 
-    return item.category === selectedCategory;
-  });
-
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+    return (
+      <div className="ShoppingList">
+        <div className="Filter">
+          <select name="filter" onChange={this.handleCategoryChange}>
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+        </div>
+        <ul className="Items">
+          {itemsToDisplay.map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ))}
+        </ul>
       </div>
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default ShoppingList;
